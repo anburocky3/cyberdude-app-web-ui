838211c4baf7580a15726db42ec9a25894750e7b
pssuganthi11 <pssuganthi11@gmail.com>
2026-01-08 19:59:50 +0530

    fix: Faq section fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import SiteSkills from "@/components/ui/site-skills";
 import Testimonial from "@/components/ui/testimonial-section";
 import WhychooseSection from "@/components/ui/whychoosesection";
 import SiteFooter from "@/components/ui/site-footer"
+import  FaqSection  from "@/components/ui/faq-section";
 
 export default function Home() {
   return (
@@ -97,6 +98,7 @@ export default function Home() {
         <Testimonial />
 
         {/* FAQ section */}
+        <FaqSection />
           
         <SiteFooter />
 
