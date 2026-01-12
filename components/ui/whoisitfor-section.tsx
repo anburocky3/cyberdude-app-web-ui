8c1e4d71580a45e66237cfed14520b33d36bf545
Prakash M <mprakash4678@gmail.com>
2026-01-12 16:07:12 +0530

    fix: responsiveness for who is it for section

--- components/ui/whoisitfor-section.tsx
+++ components/ui/whoisitfor-section.tsx
@@ -14,7 +14,7 @@ const WhoIsItFor = () => {
         ?
       </h1>
 
-      <div className="flex justify-center items-center mt-20">
+      <div className="flex justify-center items-center mt-20 mx-5">
         <div className="flex flex-col gap-10 md:flex-row max-w-7xl">
           {WhoForData.map((card, index) => (
             <div
