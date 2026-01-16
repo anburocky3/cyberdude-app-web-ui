c8f78efe1c257e5aa1f93bf3b3ca5ea405a6b674
Awizp <ridvishnu@gmail.com>
2026-01-16 19:07:09 +0530

    fix: Responsiveness of site and All sections UI improved

--- components/ui/hero-section.tsx
+++ components/ui/hero-section.tsx
@@ -1,79 +1,75 @@
 export default function HeroSection() {
   return (
-    <section className="flex flex-col items-center text-center text-white pt-0">
-      <h1 className="font-bold lg:font-medium text-3xl lg:text-6xl font-sans mb-3">
-        {" "}
-        Transform into{" "}
-        <span
-          className="bg-linear-to-r from-orange-300 via-amber-400 to-orange-600
-         bg-clip-text text-transparent"
-        >
-          skilled professional
-        </span>
-      </h1>
-      <p className=" text-gray-200 font-medium text-lg">
-        evidence-based learning environment where completion actually means
-        competence.
-      </p>
+    <section className="w-full">
+      <div className="container mx-auto px-3 flex flex-col text-center text-white 0 2xl:pt-20 pb-10">
+        <h1 className="font-medium text-3xl lg:text-6xl 2xl:text-7xl font-sans mb-3">
+          Transform Into
+          <span className="custom-gradient"> Skilled Professional</span>
+        </h1>
+        <p className=" text-gray-200 font-medium lg:text-lg 2xl:text-2xl mt-2">
+          Evidence based learning environment where completion actually means
+          competence.
+        </p>
 
-      <div className="flex flex-col lg:flex-row items-center justify-center mt-6 gap-2">
-        <div className="flex -space-x-3">
-          <img
-            className="w-10 h-10 rounded-full border-2 border-theme-gradient"
-            src="https://github.com/awizp.png"
-            alt="Vishnu"
-          />
+        <div className="flex items-center justify-center mt-6">
+          <div className="flex -space-x-3">
+            <img
+              className="w-10 h-10 rounded-full border-2 border-theme-gradient"
+              src="https://github.com/awizp.png"
+              alt="Vishnu"
+            />
 
-          <img
-            className="w-10 h-10 rounded-full border-2 border-theme-gradient"
-            src="https://github.com/sidhgeetha.png"
-            alt="Geetha"
-          />
-          <img
-            className="w-10 h-10 rounded-full border-2 border-theme-gradient"
-            src="https://github.com/prakashmcodes.png"
-            alt="Prakash"
-          />
+            <img
+              className="w-10 h-10 rounded-full border-2 border-theme-gradient"
+              src="https://github.com/sidhgeetha.png"
+              alt="Geetha"
+            />
+            <img
+              className="w-10 h-10 rounded-full border-2 border-theme-gradient"
+              src="https://github.com/prakashmcodes.png"
+              alt="Prakash"
+            />
+          </div>
+
+          <span className="ml-4 font-sm text-sm text-gray-300">
+            Getting skilled by{" "}
+            <strong className="text-white tracking-wide">1M+ Learners</strong>
+          </span>
         </div>
 
-        <span className=" font-sm text-sm text-gray-300">
-          Getting skilled by{" "}
-          <strong className="text-white tracking-wide">1M+ Learners</strong>
-        </span>
-      </div>
-
-      <div className=" flex flex-col  lg:flex-row gap-4 items-center text-center justify-center mt-8 px-4">
-        <button className="flex flex-row text-center items-center justify-center bg-linear-to-r from-orange-300 to-orange-500 text-black lg:text-xl font-semibold w-50 h-14 rounded-4xl hover:from-orange-400 hover:to-orange-600 cursor-pointer">
-          Start Journey
-        </button>
-        <button className="flex flex-row text-center items-center justify-center border border-white lg:text-lg w-50 h-14 rounded-4xl text-white hover:bg-linear-to-r hover:theme-gradient hover:from-blue-50 hover:to-indigo-100 hover:text-black font-semibold hover:border-none  cursor-pointer">
-          For Recruiters
-        </button>
-      </div>
-
-      <div className="w-full max-w-7xl mx-auto px-6 lg:px-10 mt-6 flex flex-row items-center justify-center gap-8">
-        <div>
-          <img
-            className=" w-44 rounded-2xl object-fit"
-            src="/img/site/heros/2.png"
-            alt="Hero Image"
-          />
+        <div className=" items-center text-center flex justify-center mt-8 space-x-4">
+          <button className="flex flex-row text-center items-center justify-center bg-linear-to-r from-orange-300 to-orange-500 text-black lg:text-xl font-semibold w-50 h-12 rounded-4xl hover:from-orange-400 hover:to-orange-600 cursor-pointer">
+            Start Journey
+          </button>
+          <button className="flex flex-row text-center items-center justify-center border border-white lg:text-lg w-50 h-12 rounded-4xl text-white hover:bg-linear-to-r hover:theme-gradient hover:from-blue-50 hover:to-indigo-100 hover:text-black font-semibold hover:border-none  cursor-pointer">
+            For Recruiters
+          </button>
         </div>
 
-        <div>
-          <img
-            className="w-72 rounded-2xl object-fit"
-            src="/img/site/heros/3.png"
-            alt="Hero Image"
-          />
-        </div>
+        <div className="w-full max-w-7xl mx-auto px-6 lg:px-10 mt-16 flex flex-row items-center justify-center gap-8">
+          <div>
+            <img
+              className=" w-44 rounded-2xl object-fit"
+              src="/img/site/heros/2.png"
+              alt="Hero Image"
+            />
+          </div>
 
-        <div>
-          <img
-            className="w-44 rounded-2xl object-fit"
-            src="/img/site/heros/5.png"
-            alt="Hero Image"
-          />
+          <div>
+            <img
+              className="w-72 rounded-2xl object-fit"
+              src="/img/site/heros/3.png"
+              alt="Hero Image"
+            />
+          </div>
+
+          <div>
+            <img
+              className="w-44 rounded-2xl object-fit"
+              src="/img/site/heros/5.png"
+              alt="Hero Image"
+            />
+          </div>
         </div>
       </div>
     </section>
--- components/ui/site-header.tsx
+++ components/ui/site-header.tsx
@@ -1,125 +1,130 @@
+"use client";
+
+import { Menu, ChevronDown, X } from "lucide-react";
+import { useState } from "react";
+
 export default function SiteHeader() {
+  const [navOpen, setNavOpen] = useState(false);
+
+  const handleNavbar = () => {
+    setNavOpen((prev) => !prev);
+  };
+
   return (
-    <header className=" flex  justify-between py-10 px-3 md:px-0 z-50">
-      <div className="container mx-auto flex">
-        <div className="w-full max-w-7xl px-10 lg:px-6 sm:px-6 flex lg:justify-start sm:justify-start">
-          <img src="/img/logo.svg" className="w-32 " alt="CyberDude Logo" />
+    <header className="w-full">
+      <div className="container mx-auto py-5 flex justify-between items-center px-3">
+        {/* company logo */}
+        <div className="w-45 h-10">
+          <img
+            src="/img/logo.svg"
+            className="w-full h-full object-contain"
+            alt="CyberDude Networks Logo"
+          />
         </div>
 
-        <div className="hidden text-white md:flex text-sm md:text-base lg:text-lg items-center justify-between space-x-6 ">
-          {/* <!-- Nav Links --> */}
-          <ul className="flex items-center space-x-12  tracking-wide">
-            <li className="flex items-center gap-1 ">
-              <a
-                href="#company"
-                className="hover:underline hover:decoration-teal-400"
-              >
-                Company
-              </a>
-              <span>
-                {" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 24 24"
+        <div className="flex gap-3 items-center justify-between">
+          {/* desktop navigation links */}
+          <div className="desktop-links">
+            <ul className="flex flex-col md:flex-row gap-10 items-start md:items-center justify-center">
+              <li>
+                <a
+                  href="#Company"
+                  className="hover:text-orange-500 transition-colors cursor-pointer duration-300 flex"
                 >
-                  <path
-                    fill="currentColor"
-                    d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6z"
-                  />
-                </svg>
-              </span>{" "}
-            </li>{" "}
-            <li>
-              <a
-                href="#learners"
-                className="hover:underline hover:decoration-teal-400 "
-              >
-                Learners
-              </a>
-            </li>
-            <li>
-              <a
-                href="#recruiters"
-                className="hover:underline hover:decoration-teal-400"
-              >
-                Recruiters
-              </a>
-            </li>
-            <li>
-              <a
-                href="#pricing"
-                className="hover:underline hover:decoration-teal-400"
-              >
-                Pricing
-              </a>
-            </li>
-            <li className="">
-              {/* <a
-              href=""
-              className="block w-36 bg-white text-black px-4 py-2 rounded hover:bg-gray-200 font-medium"
-            >
-              Get Started!
-            </a> */}
-              <a
-                href=""
-                className="block w-36  px-4 py-2 rounded-full bg-linear-to-r from-orange-300  to-orange-500 text-black font-medium hover:from-orange-400 hover:to-orange-500 transition-colors duration-300"
-              >
-                Get Started!
-              </a>
-            </li>
-          </ul>{" "}
-        </div>
+                  <ChevronDown />
+                  <span className="ml-1">Company</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#Learners"
+                  className="hover:text-orange-500 transition-colors cursor-pointer duration-300"
+                >
+                  Learners
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#Recruiters"
+                  className="hover:text-orange-500 transition-colors cursor-pointer duration-300"
+                >
+                  Recruiters
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#Pricing"
+                  className="hover:text-orange-500 transition-colors cursor-pointer duration-300"
+                >
+                  Pricing
+                </a>
+              </li>
+            </ul>
+          </div>
 
-        <div className="md:hidden ">
-          <input type="checkbox" id="menuToggle" className="peer hidden" />
-
-          <label
-            htmlFor="menuToggle"
-            className=" cursor-pointer flex items-center justify-center relative z-50"
+          {/* get started button */}
+          <a
+            href="#"
+            className="w-full ml-5 px-4 py-2 rounded-full bg-linear-to-r from-orange-300  to-orange-500 text-black font-medium hover:from-orange-400 hover:to-orange-500 transition-colors duration-300 flex justify-center items-center"
           >
-            <svg
-              className="block w-6 h-6 text-white peer-checked:hidden"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />{" "}
-            </svg>{" "}
-            <svg
-              className="hidden peer-checked:block w-6 h-6 text-white "
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />{" "}
-            </svg>
-          </label>
+            Get Started!
+          </a>
 
-          {/* <!-- Mobile Menu --> */}
-          <nav className="hidden peer-checked:flex flex-col absolute justify-center text-center top-full left-0 w-full bg-black text-white space-y-4 p-4 md:hidden">
-            <a href="#">Company</a> <a href="#">Learners</a>{" "}
-            <a href="#">Recruiters</a> <a href="#">Pricing</a>{" "}
-            <a
-              href="#"
-              className="text-black bg-white px-4 py-2 rounded hover:bg-gray-200 font-medium mx-auto"
-            >
-              Login
-            </a>
-          </nav>
+          {/* hamburger menu button */}
+          <div
+            onClick={handleNavbar}
+            className="hover:text-orange-500 transition-colors cursor-pointer duration-300 md:hidden"
+          >
+            <Menu size={34} />
+          </div>
+
+          {/* mobile navigation links */}
+          {navOpen ? (
+            <div className="mobile-links">
+              <ul className="flex flex-col md:flex-row gap-10 items-start md:items-center justify-center">
+                <li>
+                  <a
+                    href="#Company"
+                    className="hover:text-orange-500 transition-colors cursor-pointer duration-300 flex"
+                  >
+                    <ChevronDown />
+                    <span className="ml-1">Company</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#Learners"
+                    className="hover:text-orange-500 transition-colors cursor-pointer duration-300"
+                  >
+                    Learners
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#Recruiters"
+                    className="hover:text-orange-500 transition-colors cursor-pointer duration-300"
+                  >
+                    Recruiters
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#Pricing"
+                    className="hover:text-orange-500 transition-colors cursor-pointer duration-300"
+                  >
+                    Pricing
+                  </a>
+                </li>
+              </ul>
+
+              {/* close navigation button */}
+              <div onClick={handleNavbar} className="close-btn">
+                <X size={34} />
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </header>
